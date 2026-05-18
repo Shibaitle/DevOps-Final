2b849cd1657989ad21bf0a682e3faf8c2b02a4ff
Shibaitle <shiba.despacito@gmail.com>
2026-05-18 14:21:57 +0700

    remove unused profile image URL from the set

--- devops-final-fe/src/lib/profile-image.ts
+++ devops-final-fe/src/lib/profile-image.ts
@@ -2,7 +2,6 @@ export const DEFAULT_PROFILE_IMAGE = "/images/profile.png";
 
 const DEFAULT_PROFILE_IMAGE_URLS = new Set([
   DEFAULT_PROFILE_IMAGE,
-  "https://www.isranews.org/article/images/2025/Harry/6/Hun_Sen_July_2019.jpg",
 ]);
 
 export function resolveProfileImage(url?: string | null): string | undefined {
